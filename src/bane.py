40a1628fe1c4bdfb6a96c804eb845f80849fd97d
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2018-11-14 19:54:52 +0000

    Update bane.py

--- src/bane.py
+++ src/bane.py
@@ -7,17 +7,25 @@ from sklearn.decomposition import TruncatedSVD
 
 class BANE(object):
     """
+    Binarized Attributed Network Embedding Class (ICDM 2018).
+    
     """
-    def __init__(self,args, P, X):
+    def __init__(self, args, P, X):
         """
+        Setting up a BANE model.
+        :param args: Arguments object.
+        :param P: Normalized connectivity matrix.
+        :param X: Feature matrix.
         """
         self.args = args
         self.P = P
         self.X = X
 
-
     def fit(self):
         """
+        Creating a BANE embedding.
+        1. Running SVD.
+        2. Running power iterations and CDC.
         """
 
         print("\nFitting BANE model.\nBase SVD fitting started.")
@@ -31,6 +39,7 @@ class BANE(object):
 
     def fit_base_SVD_model(self):
         """
+        Reducing the dimensionality with SVD in the 1st step.
         """
         self.P = self.P.dot(self.X)
         self.model = TruncatedSVD(n_components=self.args.dimensions, n_iter = 70, random_state = 42)
@@ -40,17 +49,20 @@ class BANE(object):
 
     def update_G(self):
         """
+        Uopdating the kernel matrix.
         """
 
         self.G = inv(np.dot(self.B.transpose(), self.B)+self.args.alpha*np.eye(self.args.dimensions)).dot(self.B.transpose()).dot(self.P)
 
     def update_Q(self):
         """
+        Updating the rescaled target matrix.
         """
         self.Q = self.G.dot(self.P.transpose()).transpose()
 
     def update_B(self):
         """
+        Updating the embedding matrix.
         """
         for i in tqdm(range(self.args.approximation_rounds), desc='Inner approximation:'):
             for dimension in range(self.args.dimensions):
@@ -59,6 +71,7 @@ class BANE(object):
 
     def binary_optimize(self):
         """
+        Starting 2nd optimization phase with power iterations and CDC.
         """
 
         self.B = np.sign(np.random.normal(size=(self.P.shape[0], self.args.dimensions)))
@@ -70,6 +83,7 @@ class BANE(object):
 
     def save_embedding(self):
         """
+        Saving the embedding.
         """
         self.out = np.concatenate([np.array(range(self.B.shape[0])).reshape(-1,1),self.B],axis=1)
         self.out = pd.DataFrame(self.out,columns = ["id"] + [ "x_"+str(dim) for dim in range(self.args.dimensions)])
